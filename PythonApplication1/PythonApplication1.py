756c05e61fe348d6314e73792c263063f5d447b0
tomasAtrat <tomasatrat@gmail.com>
2021-06-01 13:23:59 -0300

    Nuevas funciones

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,7 +1,35 @@
 def miFuncion():
-    contador= 0
+    contador = 0
     for i in list(range(10)):
         contador+= 1
     return contador
 
 print(miFuncion())
+
+#matriz = [1, 2, 3, 4, [5, 6, 7]]
+#nombre = int(input("Escriba un número"))
+#matriz.append(nombre)
+
+#def printMatriz(matriz):
+#    for x in matriz.count:
+#        pass
+
+def funcWhile():
+    iterador=0
+    while iterador<20:
+        print(f"El iterador vale {iterador}")
+        iterador+=1
+
+def NumerosIguales(x, y):
+    if(x>y):
+        return x
+    elif(y>x):
+        return y
+    else:
+        return "Son iguales"
+
+result= NumerosIguales(20, 20)
+
+print(result)
+
+
